9d0a5d32f590da941139c9b3dddb99fa6588599a
Ray Nicholus <rnicholus+github@widen.com>
2014-07-04 04:00:42 +0000

    feat($validation): respect field validation
    ...also listen for invalid events on fields, aggregate them
    and fire an "invalid" event on the form with all invalid fields
    as event detail.
    
    closes #5

--- element/ajax-form.js
+++ element/ajax-form.js
@@ -3,46 +3,81 @@
         if (method) {
             var proposedMethod = method.toLowerCase();
 
+            // PUT & POST are the only acceptable methods for now.  
+            // GET requires us to be able to convert form fields into a URL-encoded string.
             if (['put', 'post'].indexOf(proposedMethod) >= 0) {
                 return proposedMethod;
             }
         }
     },
 
-    sendForm = function() {
+    sendForm = function(form) {
         var sender = this.shadowRoot.querySelector('core-ajax');
 
         // make sure Polymer/core-ajax doesn't touch the Content-Type.
         // The browser must set this with the proper multipart boundary ID.
         sender.contentType = null;
 
-        sender.body = new FormData(window.unwrap(this));
+        sender.body = new FormData(window.unwrap(form));
         sender.go();
+    },
+    
+    watchForInvalidFields = function(form) {
+        var customEl = this,
+            fields = form.children,
+            invalidFields = [],
+            timer = null;
+        
+        for (var i = 0; i < fields.length; i++) {
+            /* jshint loopfunc:true */
+            fields[i].addEventListener('invalid', function() {
+                invalidFields.push(this);
+
+                // In case another element is invalid and the event 
+                // hasn't been triggered yet, hold off on firing the 
+                // invalid event on the custom el.
+                clearTimeout(timer);
+                timer = setTimeout(function() {
+                    customEl.fire('invalid', invalidFields);
+                    invalidFields = [];
+                    console.error('Form submission blocked - constraints violation.');
+                }, 10);
+            });
+        }
+        
     };
 
     /* globals Polymer */
     /* jshint newcap: false */
     Polymer('ajax-form', {
         acceptableMethod: 'post', //just a default value
-        
+
         domReady: function() {
-            // The method attribute set on the light-DOM `<form>` 
-            // can't seem to be access as a property of this element, 
-            // unlike other attributes.  Perhaps due to the fact that 
-            // we are extending a form and a "natural" form also has a 
+            // The method attribute set on the light-DOM `<form>`
+            // can't seem to be access as a property of this element,
+            // unlike other attributes.  Perhaps due to the fact that
+            // we are extending a form and a "natural" form also has a
             // method attr?  Need to look into this further.
             this.acceptableMethod = getValidMethod(this.getAttribute('method'));
-            
+
             if (!this.acceptableMethod) {
                 throw new Error('Invalid method!');
             }
 
+            watchForInvalidFields.call(this, this);
+
             this.addEventListener('submit', function(event) {
-                // stop form submission
+                // Stop form submission.  Believe it or not, 
+                // both of these are required for some reason, 
+                // and returning false doesn't seem to reliably work.
                 event.preventDefault();
                 event.stopPropagation();
 
-                sendForm.call(this);
+                // respect any field validation attributes
+                // NOTE: Safari doesn't have any visual indications when submit is blocked
+                if (this.checkValidity()) {
+                    sendForm.call(this, this);
+                }
             }.bind(this));
         }
     });
--- test/unit/ajax-form-spec.js
+++ test/unit/ajax-form-spec.js
@@ -1,9 +1,9 @@
-/* globals jasmine, polymerInstance */
+/* globals Event, jasmine, polymerInstance */
 describe('ajax-form custom element tests', function() {
     beforeEach(function() {
         this.form = document.createElement('form');
     });
-    
+
     describe('method normalization', function() {
         it('throws an error if the method is not "post" or "put"', function() {
             expect(polymerInstance.spec.domReady.bind(this.form)).toThrow();
@@ -11,7 +11,7 @@ describe('ajax-form custom element tests', function() {
             this.form.setAttribute('method', 'get');
             expect(polymerInstance.spec.domReady.bind(this.form)).toThrow();
         });
-        
+
         it('adds a listener to squelch submit events if the method is "post"', function() {
             spyOn(this.form, 'addEventListener');
 
@@ -26,6 +26,46 @@ describe('ajax-form custom element tests', function() {
             this.form.setAttribute('method', 'put');
             polymerInstance.spec.domReady.call(this.form);
             expect(this.form.addEventListener.calls.mostRecent().args[0]).toBe('submit');
+        });
+    });
+
+    describe('form validation', function() {
+        beforeEach(function() {
+            jasmine.clock().install();
+            
+            this.form.setAttribute('method', 'post');
+            this.form.fire = jasmine.createSpy('fire');
+        });
+        
+        afterEach(function() {
+            jasmine.clock().uninstall();
+        });
+
+        it('ensures invalid fields are idenitified and presented via a single event', function() {
+            var textInput1 = document.createElement('input'),
+                textInput2 = document.createElement('input'),
+                textInput3 = document.createElement('input');
+
+            textInput1.setAttribute('name', 'test1');
+            textInput2.setAttribute('name', 'test2');
+            textInput3.setAttribute('name', 'test3');
+
+            this.form.appendChild(textInput1);
+            this.form.appendChild(textInput2);
+            this.form.appendChild(textInput3);
+
+            polymerInstance.spec.domReady.call(this.form);
+            
+            var inputInvalidEvent = document.createEvent('Event');
+            inputInvalidEvent.initEvent('invalid', true, true);
+            
+            textInput1.dispatchEvent(inputInvalidEvent);
+            jasmine.clock().tick(5);
+            
+            textInput3.dispatchEvent(inputInvalidEvent);
+            jasmine.clock().tick(10);
+
+            expect(this.form.fire).toHaveBeenCalledWith('invalid', [textInput1, textInput3]);
         });
     });
 });
