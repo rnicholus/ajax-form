1c6443d691c5edfcc73c5b3eff7c570528a168cf
Ray Nicholus <rnicholus+github@widen.com>
2014-07-08 01:57:16 +0000

    chore($lint): fix lint error not caught due to bad lint path

--- grunt_tasks/jshint.js
+++ grunt_tasks/jshint.js
@@ -2,7 +2,7 @@
 /* globals module */
 module.exports = {
     files: [
-        'element/*.js',
+        'ajax-form.js',
         'gruntfile.js',
         'grunt_tasks/*.js',
         'test/unit/*.js'
